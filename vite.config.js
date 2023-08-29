a133cfa5438fec3b4a24675d81fca7a6ef137451
Youting <boy80314@gmail.com>
2023-08-29 14:25:50 +0800

    setting base

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/layout-week-6/',
+  base: '/week-6-aos/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
